28953499706402f9ec4aa35c1ccdb1f8fa7c558b
Julien FABRE <julien.fabre.33@gmail.com>
2019-03-29 00:03:04 +0100

    ajout en cours...

---
+++ adresse-api-spring/src/main/java/fr/jufab/distributed/tracing/adresse/AdresseApiSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.jufab.distributed.tracing.adresse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdresseApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdresseApiSpringApplication.class, args);
+	}
+
+}
