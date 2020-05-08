27a8e00c2b068f115d7d856222697923866eec6d
Julien Fabre <julien.fabre@pole-emploi.fr>
2020-05-08 14:04:18 +0200

    del: supp projet en trop

--- personne-api-thorntail/src/main/java/fr/jufab/distributed/tracing/PersonneAPIApplication.java
+++
@@ -1,9 +0,0 @@
-package fr.jufab.distributed.tracing;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class PersonneAPIApplication extends Application {
-
-}
